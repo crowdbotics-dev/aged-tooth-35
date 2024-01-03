1ea2596ed3ff51bdde4c192643881cbb47349c66
Crowdbotics <engineering@crowdbotics.com>
2024-01-03 07:57:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-tooth-35",
+  url: "https://aged-tooth-35.botics.co",
+  api: "https://aged-tooth-35.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
